558b450b2c74e9146652f97b2fac1c9a75644cd8
dilipSunar123 <rexsunar37010@gmail.com>
2024-02-29 11:40:32 +0530

    Added Maximize Window in Test-01

--- Scripts/Test-01/Script1709120961621.groovy
+++ Scripts/Test-01/Script1709120961621.groovy
@@ -19,6 +19,8 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+
 WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
 
 WebUI.verifyElementPresent(findTestObject('Object Repository/Test_OR_01/Page_CURA Healthcare Service/h1_CURA Healthcare Service'), 
